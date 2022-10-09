64104918229bb2ad6d43524733ff32e5f48db092
JanusZhu <janus.zhu@yale.edu>
2022-10-10 02:54:15 +0900

    adding html page

--- app.js
+++ app.js
@@ -3,6 +3,46 @@ function getComputerChoice() {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+function changePlayerPoint() {
+  playerPoint += 1;
+  const point = document.querySelector("#player-points");
+  const number = Number(point.innerHTML);
+  point.innerHTML = (number + 1).toString();
+}
+
+function changeComputerPoint() {
+  computerPoint += 1;
+  const point = document.querySelector("#computer-points");
+  const number = Number(point.innerHTML);
+  point.innerHTML = (number + 1).toString();
+}
+
+function resetEverything() {
+  document.getElementById("text").innerHTML = null;
+  document.getElementById("result").innerHTML = null;
+  document.getElementById("winner").innerHTML = null;
+  document.getElementById("computer-points").innerHTML = "0";
+  document.getElementById("player-points").innerHTML = "0";
+  disableButtons(false);
+  playerPoint = 0;
+  computerPoint = 0;
+}
+
+function changeText(newText) {
+  document.getElementById("text").innerHTML = newText;
+}
+
+function changeResult(newText) {
+  document.getElementById("result").innerHTML = newText;
+}
+function anounceWinner(newText) {
+  document.getElementById("winner").innerHTML = newText;
+}
+function disableButtons(value) {
+  const buttons = Array.from(document.querySelectorAll(".button"));
+  buttons.forEach((button) => (button.disabled = value));
+}
+
 function playRound(playerSelection, computerSelection) {
   // your code here!
   if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
@@ -28,39 +68,42 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let result = 0;
-  for (let i = 0; i < 5; i++) {
-    let array = ["rock", "paper", "scissors"];
-    let playerSelection = prompt("What's your choice? (rock, paper, scissors)");
-    while (true) {
-      if (array.includes(playerSelection.toLowerCase())) {
-        break;
-      }
-      playerSelection = prompt("Choose a valid option (rock, paper, scissors)");
-    }
-    const computerSelection = getComputerChoice();
-    console.log(
-      "Round " +
-        (i + 1) +
-        ": You choose " +
-        playerSelection.toLowerCase() +
-        ". The computer choose " +
-        computerSelection.toLowerCase() +
-        "."
-    );
-    result += playRound(playerSelection, computerSelection);
-  }
+function game(e) {
+  let playerSelection = e.target.id;
+  const computerSelection = getComputerChoice();
+  const text =
+    "You choose " +
+    playerSelection.toLowerCase() +
+    ". The computer choose " +
+    computerSelection.toLowerCase() +
+    ".";
+  changeText(text);
+  let result = playRound(playerSelection, computerSelection);
   if (result > 0) {
-    console.log("You win.");
+    changePlayerPoint();
+    changeResult("You win this round.");
   } else if (result < 0) {
-    console.log("You lose.");
+    changeComputerPoint();
+    changeResult("You lose this round.");
   } else {
-    console.log("It's a tie.");
+    changeResult("It's a tie.");
+  }
+  if (playerPoint == 5) {
+    anounceWinner("The winner is the player!");
+    disableButtons(true);
+  } else if (computerPoint == 5) {
+    anounceWinner("The winner is the computer!");
+    disableButtons(true);
   }
 }
+let playerPoint = 0;
+let computerPoint = 0;
+const buttons = Array.from(document.querySelectorAll(".button"));
+buttons.forEach((button) => button.addEventListener("click", game));
 
-game();
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetEverything);
+console.log(playerPoint);
 
 //console.log(computerSelection);
 //console.log(playRound(playerSelection, computerSelection));
